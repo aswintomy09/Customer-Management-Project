6326b4079f61756507e26156b5a5f2e784a7e928
aswintomy <aswintomy09@gmail.com>
2024-08-08 18:24:38 +0530

    Fixed order service.

--- Backend/customer-management/src/main/java/com/customer/application/repository/CustomerRepository.java
+++ Backend/customer-management/src/main/java/com/customer/application/repository/CustomerRepository.java
@@ -1,19 +1,11 @@
 package com.customer.application.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
-
 import com.customer.application.entity.Customer;
-
-import java.util.List;
-
-import static com.customer.application.constants.Constants.GET_CUSTOMERS_WITH_ORDER_QUERY;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer,Long>{
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    @Query(GET_CUSTOMERS_WITH_ORDER_QUERY)
-    List<Customer> findAllWithOrders();
 }
